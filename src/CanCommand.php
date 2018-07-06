4e6665a8eaa124d0ee5f95ae9665a0f749024d2b
Phil Everton <phil@inclind.com>
2018-07-06 07:51:07 -0400

    updated to coding standards

--- src/CanCommand.php
+++ src/CanCommand.php
@@ -1,7 +1,6 @@
 <?php
 namespace Pantheon\TerminusCantilever;
 
-//use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Pantheon\Terminus\Commands\Site\SiteCommand;
 
 class CanCommand extends SiteCommand
@@ -20,46 +19,46 @@ class CanCommand extends SiteCommand
    * example: terminus can --env=live --level='pro,business,performance' --frame='drupal,drupal8' --command='pml|grep redis'
    *
    */
-  public function cantilever($options = ['env' => 'dev', 'level' => null, 'frame' => null, 'command' => null])
-  {
-    //show initialize
-    $this->log()->notice("Cantilever initializing...");
+    public function cantilever($options = ['env' => 'dev', 'level' => null, 'frame' => null, 'command' => null])
+    {
+        //show initialize
+        $this->log()->notice("Cantilever initializing...");
+    
+        //list sites
+        $sites = $this->sites->serialize();
+    
+        if (empty($sites)) {
+            $this->log()->notice('You have no sites.');
+        }
 
-    //list sites
-    $sites = $this->sites->serialize();
-
-    if (empty($sites)) {
-	    $this->log()->notice('You have no sites.');
+        foreach ($sites as $key => $site) {
+            if (isset($options['level'])) {
+                $level = explode(",", $options['level']);
+                if (!in_array($site['service_level'], $level, true)) {
+                    unset($sites[$key]);
+                }
+            }
+      
+            if (isset($options['frame'])) {
+                $frame = explode(",", $options['frame']);
+                if (!in_array($site['framework'], $frame, true)) {
+                    unset($sites[$key]);
+                }
+            }
+      
+            //print output
+            if (isset($sites[$key])) {
+                echo $site['name']."\n";
+                if (isset($options['command'])) {
+                    echo "----------\n";
+                    $query = "drush @".$site['name'].".".$options['env']." ".$options['command'];
+                    $output = shell_exec($query);
+                    if ($output == '') {
+                        $output = "** no results **\n";
+                    }
+                    echo $output."\n";
+                }
+            }
+        }
     }
-
-    foreach ($sites as $key => $site) {
-	    if (isset($options['level'])) {
-		    $level = explode(",",$options['level']);
-		    if (!in_array($site['service_level'], $level, true)) {
-			    unset($sites[$key]);
-		    }
-	    }
-
-	    if (isset($options['frame'])) {
-		    $frame = explode(",",$options['frame']);
-		    if (!in_array($site['framework'], $frame, true)) {
-			    unset($sites[$key]);
-		    }
-	    }
-
-	    //print output
-	    if (isset($sites[$key])) {
-		    echo $site['name']."\n";
-		    if (isset($options['command'])) {
-		      echo "----------\n";
-			    $query = "drush @".$site['name'].".".$options['env']." ".$options['command'];
-			    $output = shell_exec($query);
-			    if ($output == '') {
-			      $output = "** no results **\n";
-			    }
-			    echo $output."\n";
-		    }
-	    }
-    }
-  }
 }
